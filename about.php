7734729bea8d5f6f8e45a7dd43dfb5918d1ab203
sujalmdhr <73207843+sujalmdhr@users.noreply.github.com>
2023-12-07 10:21:52 +0545

    Add files via upload

---
+++ about.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Online Hotel.Com</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <link href="css/style.css"rel="stylesheet"/>
+  <link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body style="margin-top:50px;">
+<?php
+      include('Menu Bar.php')
+  ?><br>
+<div class="container-fluid text-center">
+<div class="container"> 
+  <div class="row content">
+    <div class="col-sm-12">
+      <h1 id="font">{ CROWN HOTEL }</h1><br>
+      <p class="text-justify"><h4>Crown Hotel, situated in Ravi Bhavan, built in modern and unique style with excellent state of the art facilities, is an iconic 4-star luxury hotel that captures the essence of legendary Kathmandu valley, offering a breathtaking panoramic view of the surrounding mountains and one of the prominent world heritage site, Swayambhunath, exposing peace and tranquility. We are inspired by our caring attitude for our guests, people and the environment.
+          The hotel offers 131 well-appointed fabulous rooms and suites. The 4-star accommodation combines modern design with a host of premium amenities and facilities with thoughtful services to make each stay a unique and personalized experience.
+          The story of Crown Hotel begins with a dream of its owners to build a 4-star deluxe hotel in Nepal providing luxury services to discerning clients, celebrating Nepalese warmth and adding one more milestone in the hospitality industry.
+
+          Our distinctive feature is our highly motivated and well-trained staff who provide exceptionally attentive, personalized and warm service.</p></h4>
+<h1 align="center"id="font">[ Services ]</h1><br>
+        <p class="text-justify"><h4>Full-service hotels often provide a wide array of guest services and on-site facilities. Commonly found amenities may include: on-site food and beverage (room service and restaurants), meeting and conference services and facilities, fitness center, and business center. Full-service hotels range in quality from mid-scale to luxury. This classification is based upon the quality of facilities and amenities offered by the hotel.</p></h4>
+    </div>
+  </div><br><br>
+  <div class="row"align="center">
+  <!--map Start Here-->
+  <div class="col-sm-2"style="background-color:#27303b;height:80px;width:100px;">
+      <a href="#" type="button" data-toggle="modal" data-target="#myModal"><img src="image/icon/icon-01.png"width="60px;"height="50px;"style="margin-top:15px;"></a>
+  </div>
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+      </div>
+      <div class="modal-body">
+        <iframe class="frame"src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3502.9012415990155!2d77.37085911440647!3d28.602739392180986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce57ca5ff4435%3A0x6de631b7a5bfdacb!2sUnion+Bank+Of+India!5e0!3m2!1sen!2sin!4v1555502551059!5m2!1sen!2sin" width="550"height="300" frameborder="0" style="border:1" allowfullscreen></iframe>
+      </div>
+    </div>
+  </div>
+</div>      
+<!--Map Close Here-->
+        <div class="col-sm-2">
+          <h4><b>Address</b></h4>
+          <b>Hetauda-10,Main Road,Nepal</b>
+        </div>
+        <div class="col-sm-2"style="background-color:#27303b;height:80px;width:100px;">
+            <a href="#"><img src="image/icon/icon-02.png"width="60px;"height="50px;"style="margin-top:15px;"></a>
+        </div>
+         <div class="col-sm-2">
+          <h4><b>Phone</b></h4>
+              <b>057-5020589</b>
+        </div>
+        <div class="col-sm-2"style="background-color:#27303b;height:80px;width:100px;">
+            <a href="#"><img src="image/icon/icon-03.png"width="60px;"height="50px;"style="margin-top:15px;"></a>
+        </div>
+         <div class="col-sm-2">
+          <h4><b>Email-Id</b></h4>
+              <b>Crownhotel@gmail.com</b>
+        </div>
+  </div><br><br>
+</div>
+</div>
+<?php
+  include('Footer.php')
+?>
+</body>
+</html>
